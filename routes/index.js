81acb22ab6aa4a08c1034e032d9bf786faf1e373
alex-delia <adelia3@uwo.ca>
2024-06-12 19:58:06 -0400

    create default messages and route to new form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Message Form' });
+});
+
+router.post('/new', (req, res) => {
+  const authorName = req.body.authorName;
+  const messageText = req.body.messageText;
+  messages.push({ text: messageText, user: authorName, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
